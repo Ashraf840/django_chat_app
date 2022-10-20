d0163a82ae4b869d3809f7b29c95faaf8630dc7a
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-21 00:29:42 +0600

    Working on the UI of people & chat-body sections in the chat-room-detail page

--- roomApp/views.py
+++ roomApp/views.py
@@ -17,9 +17,13 @@ def roomsList(request):
 @login_required
 def roomDetail(request, slug):
     room = Room.objects.get(slug=slug)
+    people = [i for i in range(5)]
+    sample = [i for i in range(15)]
     context = {
         'title': room.name,
         'room': room,
+        'people': people,
+        'sample': sample,
     }
     return render(request, 'roomApp/roomDetail.html', context)
 
