f300fb7161ccd1d9515809af27a2b2dd9a0354c1
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-23 21:48:06 +0600

    Fetch room-messages from the db & display into the frontend

--- roomApp/models.py
+++ roomApp/models.py
@@ -6,6 +6,9 @@ class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
