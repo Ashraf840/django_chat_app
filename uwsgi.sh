4c2d8249ed058a759687212a6de97c4873863e7d
ashraf840 <shift2unleashed2011@gmail.com>
2022-11-05 01:02:36 +0600

    Working with uwsgi.sh file; check the logs/teachatty_uwsgi.log file exist

--- uwsgi.sh
+++ uwsgi.sh
@@ -8,7 +8,15 @@ echo "Present Dir: $PWD"
 source env/bin/activate
 pip3 install uwsgi
 
-
+# Check if '/var/lib/jenkins/workspace/teachatty_chat_app/logs/teachatty_uwsgi.log' file exist, otherwise create the file
+if [ -e /var/lib/jenkins/workspace/teachatty_chat_app/logs/teachatty_uwsgi.log ]
+then
+    echo "teachatty_uwsgi.log file exists"
+else
+    echo "teachatty_uwsgi.log file doesn't exists"
+    touch logs/teachatty_uwsgi.log
+    echo "Created the teachatty_uwsgi.log file into path: $PWD/logs/teachatty_uwsgi.log"
+fi
 
 
 sudo systemctl daemon-reload
