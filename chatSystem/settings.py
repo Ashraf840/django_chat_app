37f3d8695a7d3c01f2b847dbccf495c0872e8c0f
tanjim840 <tanjim.techforing@gmail.com>
2022-11-05 14:09:52 +0600

    Working with nginx.sh file; created teachatty_nginx.conf file

--- chatSystem/settings.py
+++ chatSystem/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-3&+(=hjoth5rxdyor$pwciqlj=4@km=ms_@-jxpzlfw-jm7r4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'www.teachatty.xyz',
+    '3.109.85.86',
+    '127.0.0.1',
+]
 
 # NB: Taking the name alias of the controllers
 LOGOUT_REDIRECT_URL = 'login'
